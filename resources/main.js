be440ebad60936e16b784cce52e678df023f0ac1
Jon <jsz002@ucsd.edu>
2020-03-19 23:16:47 -0700

    DONE DONE

--- resources/main.js
+++ resources/main.js
@@ -1,22 +1,21 @@
-function getChart(name, name1, name2, data, data2) {
+function getChart(name, name1, name2, data1, data2) {
     var ctx = document.getElementById(`${name}Chart`).getContext('2d');
     var chart = new Chart(ctx, {
         type: 'radar',
         data: {
-            // labels: ['PTS', 'REB', 'AST', 'PER', 'BLK', 'STL'],
             labels: ['PTS', 'REB', 'AST'],
             datasets: [{
                 label: name1,
-                data,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
+                data: data1,
+                backgroundColor: 'rgba(142, 195, 167, 0.5)',
+                borderColor: 'rgba(142, 195, 167, 1)',
                 borderWidth: 1,
             },
             {
                 label: name2,
                 data: data2,
-                backgroundColor: 'rgba(0, 0, 255, 0.2)',
-                borderColor: 'rgba(0, 0, 255, 0.2)',
+                backgroundColor: 'rgba(250, 203, 105, 0.5)',
+                borderColor: 'rgba(250, 203, 105, 1)',
                 borderWidth: 1,
             }]
         }
